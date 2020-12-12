63c0aa1c542328dc10d1bcfe000dd1fcd3007bfc
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2020-12-11 19:28:42 -0800

    Updated metadata

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -5,25 +5,27 @@ export default function About() {
     
   return (
       <div className="page" id="about">
-        <div className="about">
-          <div>
-            <h2>About Me</h2>
-            <p>
-                I’m a growing web developer with a mathematical brain
-                and an artistic point of view. My interest in
-                programming stemmed from my fascination with
-                the way things work and wanting to take a look behind
-                the curtain. Once I learned how to build programs in
-                highschool it gave me a new perspective and I knew that
-                this is what I wanted to do for a living. I come from a
-                family of builders and I aspire to use that foundation
-                to build new solutions that solve large scale problems in
-                the world of tech.
-            </p>
+        <div className="container">
+          <div className="about">
+            <div>
+              <h2>About Me</h2>
+              <p>
+                  I’m a growing web developer with a mathematical brain
+                  and an artistic point of view. My interest in
+                  programming stemmed from my fascination with
+                  the way things work and wanting to take a look behind
+                  the curtain. Once I learned how to build programs in
+                  highschool it gave me a new perspective and I knew that
+                  this is what I wanted to do for a living. I come from a
+                  family of builders and I aspire to use that foundation
+                  to build new solutions that solve large scale problems in
+                  the world of tech.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="profile">
-            <img src={Profile} alt="Profile"/>
+          <div className="profile">
+              <img src={Profile} alt="Profile"/>
+          </div>
         </div>
     </div>
   )

13880135b11cf645660e1e5806363669c4b3f6d9
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-01-07 13:57:57 -0800

    Added text background/animation for about me

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Profile from '../assets/profile.png';
 
 export default function About() {
+  const [matrixStyles, setMatrixStyles] = useState({display: "none"});
+
+  const fontSettings = {
+    fontRatio: 0.651,
+    numCols: 30,
+    numRows: 15
+  };
+
+  const animateMatrix = () => {
+    const matrixElem = document.getElementById("matrix");
+    setInterval(() => {
+      const matrix = matrixElem.innerHTML.split("<br>");
+      const col = Math.floor(Math.random()*matrix[0].length);
+      const row = Math.floor(Math.random()*(matrix.length-1));
+      
+      matrix[row] = matrix[row].substr(0, col) + Number(!Number(matrix[row].charAt(col))) + matrix[row].substr(col+1);
+      matrixElem.innerHTML = matrix.reduce((acc, elem) => acc + "<br>" + elem);
+    }, 10);
+  }
+
+  const initMatrix = () => {
+    const profileImage = document.getElementById("profile-image");
+    const styles = getComputedStyle(profileImage);
+    const width = Number(styles.getPropertyValue("width").slice(0, -2));
+    const height = Number(styles.getPropertyValue("height").slice(0, -2));
+    const lineHeight = height / fontSettings.numRows;
+    const fontSize = Math.round(lineHeight * 0.6);
+    const letterSpacing = width / fontSettings.numCols - fontSize * fontSettings.fontRatio;
+    setMatrixStyles({
+      lineHeight: String(lineHeight) + "px",
+      fontSize: String(fontSize) + "px",
+      letterSpacing: String(letterSpacing) + "px"
+    });
+    animateMatrix();
+  }
     
   return (
       <div className="page" id="about">
@@ -24,7 +59,24 @@ export default function About() {
             </div>
           </div>
           <div className="profile">
-              <img src={Profile} alt="Profile"/>
+            <img src={Profile} alt="Profile" id="profile-image" onLoad={initMatrix}/>
+            <p style={matrixStyles} id="matrix">
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+              000000000000000000000000000000<br/>
+            </p>
           </div>
         </div>
     </div>

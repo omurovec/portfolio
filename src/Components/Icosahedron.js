3e3920f4a101ac824419df0d8d8556b6ed52a7fb
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-01-29 09:49:30 -0800

    Fixed canvas colors on dark mode

--- src/Components/Icosahedron.js
+++ src/Components/Icosahedron.js
@@ -1,42 +1,62 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 
 export default function Icosahedron(id, size) {
-    let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera(75, size.width/size.height, 0.01, 10);
-    camera.fov = 10;
+  // Set Scene
+  let scene = new THREE.Scene();
+  let camera = new THREE.PerspectiveCamera(
+    75,
+    size.width / size.height,
+    0.01,
+    10
+  );
+  camera.fov = 10;
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setSize(size.width, size.height);
+  document.querySelector(id).appendChild(renderer.domElement);
 
-    let bgColor = new THREE.Color(0xf8f8f8);
-    scene.background = bgColor;
-    let renderer = new THREE.WebGLRenderer();
-    renderer.setSize(size.width, size.height);
-    document.querySelector(id).appendChild(renderer.domElement);
+  // Adjust size for platform
+  const sizeDiff = size.width > 300 ? 0.005 : 0.01;
 
-    const sizeDiff = size.width > 300 ? 0.005 : 0.01;
-    let geometry = new THREE.IcosahedronGeometry(3, 1);
-    let altGeometry = new THREE.IcosahedronGeometry(3 + sizeDiff, 1);
-   
-    let edges = new THREE.EdgesGeometry(altGeometry);
-    let lines = new THREE.LineSegments(
-      edges,
-      new THREE.LineBasicMaterial({color: "#000", linewidth: 1})
-    );
-    let material = new THREE.MeshBasicMaterial({
-      color: '#f8f8f8',
-    })
+  // Match background color to page background color
+  // (ie. fix colors for dynamic darkmode systems)
+  let pageBg = window
+    .getComputedStyle(document.querySelector('html'))
+    .getPropertyValue('background-color');
+  let bgColor = new THREE.Color(pageBg);
+  scene.background = bgColor;
 
-    let icosahedron = new THREE.Mesh(geometry, material)
-    scene.add(lines);
-    scene.add(icosahedron);
-    camera.position.z = 5;
-    var animate = function() {
-      requestAnimationFrame(animate);
+  // Contrast wireframe color
+  const [R, G, B] = pageBg.slice(4, pageBg.length - 1).split(',');
+  let frameColor = R > 200 || G > 200 || B > 200 ? '#000' : '#FFF';
 
-      icosahedron.rotation.y += 0.001;
-      icosahedron.rotation.x += 0.001;
-      lines.rotation.y += 0.001;
-      lines.rotation.x += 0.001;
+  // Setup Geometry
+  let geometry = new THREE.IcosahedronGeometry(3, 1);
+  let altGeometry = new THREE.IcosahedronGeometry(3 + sizeDiff, 1);
+  let edges = new THREE.EdgesGeometry(altGeometry);
+  let lines = new THREE.LineSegments(
+    edges,
+    new THREE.LineBasicMaterial({ color: frameColor, linewidth: 1 })
+  );
+  let material = new THREE.MeshBasicMaterial({
+    color: bgColor,
+  });
+  let icosahedron = new THREE.Mesh(geometry, material);
 
-      renderer.render(scene, camera);
-    };
-    animate();
+  // Attach to Scene
+  scene.add(lines);
+  scene.add(icosahedron);
+  camera.position.z = 5;
+
+  // Animate
+  var animate = function() {
+    requestAnimationFrame(animate);
+
+    icosahedron.rotation.y += 0.001;
+    icosahedron.rotation.x += 0.001;
+    lines.rotation.y += 0.001;
+    lines.rotation.x += 0.001;
+
+    renderer.render(scene, camera);
+  };
+  animate();
 }

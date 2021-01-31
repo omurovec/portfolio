32b4cf5fad2e7c91fddaa27ca242457efa80f8ae
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-01-30 22:58:19 -0800

    Mobile swipe carousel update

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,3 @@
-export {default as Icosahedron} from "./Icosahedron.js";
-export {default as Header} from "./Header.js";
+export { default as Icosahedron } from './Icosahedron.js';
+export { default as Header } from './Header.js';
+export { default as Carousel } from './Carousel.js';
--- src/Screens/Portfolio.js
+++ src/Screens/Portfolio.js
@@ -1,27 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Background from '../assets/background.png';
-import ArrowLeft from '../assets/arrow-left.svg';
-import ArrowRight from '../assets/arrow-right.svg';
 import iPhone from '../assets/iPhone-body.png';
 import Link from '../assets/link.svg';
 import { firestore, storage } from '../Firebase';
+import { Carousel } from '../Components';
 
 export default function Portfolio() {
-  return (
-    <div className="page" id="portfolio">
-      <div className="container">
-        <h1>Portfolio</h1>
-        <div className="slide">
-          <img className="background" src={Background} alt="background" />
-          <Carousel />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-const Carousel = (data) => {
-  const [panelInd, setPanelInd] = useState(0);
   const [entries, setEntries] = useState(0);
 
   useEffect(() => {
@@ -46,53 +30,48 @@ const Carousel = (data) => {
     };
   }, [entries]);
 
-  function incPanel(i) {
-    if ((panelInd === 0) & (i < 0)) {
-      setPanelInd(entries.length - 1);
-    } else if ((panelInd === entries.length - 1) & (i > 0)) {
-      setPanelInd(0);
-    } else {
-      setPanelInd(panelInd + i);
-    }
-  }
-
-  return entries ? (
-    <div className="panel">
-      <div className="phone-container">
-        <img
-          src={ArrowLeft}
-          className="arrow left"
-          alt="arrowL"
-          onClick={() => incPanel(-1)}
-        />
-        <img src={iPhone} className="phone-body" alt="iphone" />
-        <PhoneVideo videoRef={entries[panelInd].videoRef} />
-        <img
-          src={ArrowRight}
-          className="arrow right"
-          alt="arrowR"
-          onClick={() => incPanel(1)}
-        />
-      </div>
-      <div className="text-container">
-        <h2>{entries[panelInd].title}</h2>
-        <p>{entries[panelInd].desc}</p>
-        {entries[panelInd].links?.map((item) => (
-          <div className="link" key={item.name}>
-            <img src={Link} alt="link" />
-            <a href={item.link} target="_blank" rel="noopener noreferrer">
-              {item.name}
-            </a>
+  return (
+    <div className="page" id="portfolio">
+      <div className="container">
+        <h1>Portfolio</h1>
+        <img className="background" src={Background} alt="background" />
+        {entries ? (
+          <Carousel
+            data={entries}
+            renderItem={(entry) => (
+              <>
+                <div className="phone-container">
+                  <img src={iPhone} className="phone-body" alt="iphone" />
+                  <PhoneVideo videoRef={entry.videoRef} />
+                </div>
+                <div className="text-container">
+                  <h2>{entry.title}</h2>
+                  <p>{entry.desc}</p>
+                  {entry.links?.map((item) => (
+                    <div className="link" key={item.name}>
+                      <img src={Link} alt="link" />
+                      <a
+                        href={item.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {item.name}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
+          />
+        ) : (
+          <div className="loading-container">
+            <div className="loader" />
           </div>
-        ))}
+        )}
       </div>
-    </div>
-  ) : (
-    <div className="panel loading-container">
-      <div className="loader" />
     </div>
   );
-};
+}
 
 const PhoneVideo = ({ videoRef }) => {
   const [src, setSrc] = useState(null);

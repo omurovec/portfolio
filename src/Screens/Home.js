fcb98ddae1b28fc997d6987d3c42046b3feaeb08
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-01-31 09:30:32 -0800

    Improved Icosahedron resolution

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,48 +1,53 @@
-import React, {useEffect, useState} from 'react';
-import { Icosahedron } from '../Components'
+import React, { useEffect, useState } from 'react';
+import { Icosahedron } from '../Components';
 
 export default function Home() {
-    const [fName, setFName] = useState("");
-    const [lName, setLName] = useState("");
-    const [loading, setLoading] = useState(true);
-    const FIRST_NAME = "Owen";
-    const LAST_NAME = "Murovec";
+  const [fName, setFName] = useState('');
+  const [lName, setLName] = useState('');
+  const [loading, setLoading] = useState(true);
+  const FIRST_NAME = 'Owen';
+  const LAST_NAME = 'Murovec';
 
-    useEffect(() => {
-        if(loading) {
-            initIso();
-            setLoading(false);
-        }
-    }, [loading]);
-
-    useEffect(() => {
-        if(fName.length !== FIRST_NAME.length) {
-            setTimeout(() => {
-                setFName(fName + FIRST_NAME[fName.length]);
-            }, 50);
-        }else if(lName.length !== LAST_NAME.length) {
-            setTimeout(() => {
-                setLName(lName + LAST_NAME[lName.length]);
-            }, 50);
-        }
-    }, [fName, lName]);
-
-    function initIso() {
-      let width = window.innerWidth;
-      let height = window.innerHeight;
-      if(width >= height) {
-        Icosahedron("#canvas", {width: height/2, height: height/2});
-      } else {
-          Icosahedron("#canvas", {width: width*0.6, height: width*0.6});
-      }
+  useEffect(() => {
+    if (loading) {
+      initIso();
+      setLoading(false);
     }
+  }, [loading]);
 
-    return (
-        <div className="page" id="home">
-            <div className="name">
-                <h1>{fName}&nbsp;<br/>{lName}&nbsp;</h1>
-            </div>
-            <div id="canvas"/>
-        </div>
-    )
+  useEffect(() => {
+    if (fName.length !== FIRST_NAME.length) {
+      setTimeout(() => {
+        setFName(fName + FIRST_NAME[fName.length]);
+      }, 50);
+    } else if (lName.length !== LAST_NAME.length) {
+      setTimeout(() => {
+        setLName(lName + LAST_NAME[lName.length]);
+      }, 50);
+    }
+  }, [fName, lName]);
+
+  function initIso() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    if (width >= height) {
+      Icosahedron('#canvas', { width: height / 2, height: height / 2 });
+    } else {
+      Icosahedron('#canvas', { width: width, height: width });
+      document.getElementById('canvas').style.transform = 'scale(0.6)';
+    }
+  }
+
+  return (
+    <div className="page" id="home">
+      <div className="name">
+        <h1>
+          {fName}&nbsp;
+          <br />
+          {lName}&nbsp;
+        </h1>
+      </div>
+      <div id="canvas" />
+    </div>
+  );
 }
